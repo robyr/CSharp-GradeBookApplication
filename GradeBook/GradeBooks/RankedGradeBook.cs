decabeef75ad338013cc166b74c3bac072bbb758
Ryan Roby <rroby@aota.org>
2019-09-26 14:54:54 -0400

    fx

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,15 +32,16 @@ namespace GradeBook.GradeBooks
                 return 'B';
             }
             
-            else if(grades[threshold * 3] - 1 <= averageGrade)
+            else if(grades[(threshold * 3) - 1]  <= averageGrade)
             {
                 return 'C';
             }
 
-            else if (grades[threshold * 4] - 1 <= averageGrade)
+            else if (grades[(threshold * 4) -1]  <= averageGrade)
             {
                 return 'D';
             }
+
             else
             {
                 return 'F';
